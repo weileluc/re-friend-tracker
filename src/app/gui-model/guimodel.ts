688f1f15163ba7b179276f519895e2b6049925e1
zanolkev <38752085+zanolkev@users.noreply.github.com>
2018-05-02 22:17:34 +0200

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "der gerät",
             "formList": [
                 {
                     "id": "FriendForm",
